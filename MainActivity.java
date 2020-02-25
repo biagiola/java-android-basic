ac3e5b82e4982841ebcf08f60781ff36923aa3f0
David <41311088+biagiola@users.noreply.github.com>
2020-02-25 19:29:57 -0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
@@ -10,43 +11,97 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // variables are also call fields in java
     private EditText userInput;
-    private Button button;
     private TextView textView;
-    private int numTimesClicked = 0;
+    private Button button;
+    private static final String TAG = "MainActivity";
+    private final String TEXT_CONTENTS = "TextContents";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d(TAG, "onCreate: in");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.note_layout);
 
-        /* R = resources;
-        *  the parenthesis in each field (variable) has its own widget identifier that behaves like a caster.
-        * */
-        userInput = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.button);
-        textView = (TextView) findViewById(R.id.textView);
+        userInput =  findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.addButton);
+
+        userInput.setText("");
         textView.setText("");
-        // textView by default does not supper scrolling
-        // we also must to activate vertical scrolling on our blueprint
         textView.setMovementMethod(new ScrollingMovementMethod());
 
         View.OnClickListener ourOnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                numTimesClicked = numTimesClicked + 1;
-                String result = "The button got tapped " + numTimesClicked + " time";
-
-                if (numTimesClicked !=1 ) {
-                    result += "s"; // we add a s to the end;
-                }
-
+                String result = userInput.getText().toString();
+                result = result + "\n";
                 textView.append(result);
-                textView.append("\n");
+                userInput.setText("");
+
             }
         };
-
         button.setOnClickListener(ourOnClickListener);
+
+        Log.d(TAG, "onCreate: out");
+
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+
+        //we retrieve the value that was store in the bundle for our text context
+        String savedString = savedInstanceState.getString(TEXT_CONTENTS);
+        textView.setText(savedString);
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
+        super.onRestart();
+        Log.d(TAG, "onRestart: out");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        //saving the current value of the text video into the bundle
+        outState.putString(TEXT_CONTENTS, textView.getText().toString());
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: in");
+        super.onResume();
+        Log.d(TAG, "onResume: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
     }
 }
+
